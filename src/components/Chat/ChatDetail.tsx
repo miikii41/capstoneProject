a186b68e4c2888f10f303d3a0b5d9facb33e581c
Doyoon Kim <130213754+doyoom@users.noreply.github.com>
2024-12-03 20:33:25 +0900

    채팅

--- src/components/Chat/ChatDetail.tsx
+++ src/components/Chat/ChatDetail.tsx
@@ -76,7 +76,7 @@ const ChatDetail = ({ route }) => {
       <View style={styles.inputContainer}>
 
         <TouchableOpacity
-        onPress={() => navigation.navigate('ClosetMain')} >
+        onPress={() => navigation.navigate('AddCloset')} >
 
           <Image
             source={require('../../assets/Closet/hanger.png')}
